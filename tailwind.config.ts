7eae14e883774696d1357e793731ff65b617a480
jahidhasan136 <jh3495689@gmail.com>
2025-01-27 10:43:18 +0600

    halinas2 project tailwind setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: [
@@ -8,9 +9,37 @@ export default {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1rem",
+          md: "1rem",
+          lg: "4rem",
+          xl: "7rem",
+          // "2xl": "8rem",
+        },
+        screens: {
+          "2xl": "1940px",
+          xl: "1440px",
+          lg: "1024px",
+          md: "100%",
+          sm: "100%",
+        },
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#E0E721",
+        secondary: "#0057B8",
+        white: "#FFFFFF",
+        white2: "#F1F1F1",
+        black: "#000000",
+        black2: "#3E3D3D",
+      },
+      fontFamily: {
+        mulish: [
+          "Mulish", // Add Mulish as a custom font family
+          ...defaultTheme.fontFamily.sans, // Fallback to sans-serif
+        ],
       },
     },
   },
